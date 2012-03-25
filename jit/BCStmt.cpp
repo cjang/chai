6a0fdcb5e6cb04c012df4274c295469b3e38de55
Chris Jang <fastkor@gmail.com>
2012-03-25 15:04:15 -0700

    alpha 2 release, refactored JIT and first class integer arrays (deleted files)

--- jit/BCStmt.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2011 Chris Jang (fastkor@gmail.com) under The Artistic License 2.0
-
-#include "BCStmt.hpp"
-
-namespace chai_internal {
-
-BCStmt::~BCStmt(void) { }
-
-}; // namespace chai_internal
--- jit/VisitBCStmt.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2011 Chris Jang (fastkor@gmail.com) under The Artistic License 2.0
-
-#include "VisitBCStmt.hpp"
-
-namespace chai_internal {
-
-VisitBCStmt::~VisitBCStmt(void) { }
-
-}; // namespace chai_internal
